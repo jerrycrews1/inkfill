a1f9ae87e5bcd204938a0eea4186105e194332c5
Jerry Crews <jerry.crews1@gmail.com>
2020-11-09 08:51:12 -0500

    Fix an issue not allowing the last row and column to add to the matched_squares Game attributelist.  Set number of turns as a global variable instead of Game attribute to help with changing the number of turns in the init and reset_games methods.

--- inkfill.py
+++ inkfill.py
@@ -17,10 +17,11 @@ square_sz = int(500 / 25)
 pygame.init()
 clock = pygame.time.Clock()
 main_surface = pygame.display.set_mode((surface_width, surface_height))
-
+TURNS = 50
 
 class Game:
     def __init__(self):
+
         self.background_color = (0, 200, 255)
         self.board_size = (500, 500)
         self.buttons = list()
@@ -28,7 +29,7 @@ class Game:
         self.create_board()
         self.current_color = self.board[0][0].color
         self.matched_squares = [[None] * 25 for _ in range(25)]
-        self.number_of_tries_total = 25
+        self.number_of_tries_total = TURNS
         self.attempts_made = 0
 
         self.tries = list()
@@ -40,13 +41,13 @@ class Game:
             self.board.append(
                 [Square(random.choice(list_of_colors)['light'], row_i, col_i) for col_i in range(int(self.board_size[0] / 20))])
 
-    def reset_game(self):
+    def reset_game(self, color=None):
         self.matched_squares = list()
         self.board = list()
         self.create_board()
         self.current_color = self.board[0][0].color
         self.matched_squares = [[None] * 25 for _ in range(25)]
-        self.number_of_tries_total = 25
+        self.number_of_tries_total = TURNS
         self.attempts_made = 0
 
         self.tries = list()
@@ -64,21 +65,18 @@ class Game:
                                        color['light'], color['dark'], events, action=self.take_turn))
 
     def add_turns_to_screen(self, screen):
-        self.tries = [Square((128, 0, 128), 27, 1.1 * num)
-                      for num in range(self.number_of_tries_total)]
+        self.tries = [Square((128, 0, 128), 27, 1.08 * num)
+                      for num in range(self.number_of_tries_total - self.attempts_made)]
         for square in self.tries:
             screen.fill(square.color, square.rect)
 
     def take_turn(self, color):
         try:
-            self.check_winner()
             self.check_matches(color)
             self.attempts_made += 1
-            self.number_of_tries_total -= 1
             self.add_turns_to_screen(main_surface)
         except IndexError:
-            time.sleep(3)
-            self.reset_game()
+            pass
 
     def check_matches(self, color):
         if not color:
@@ -88,38 +86,48 @@ class Game:
             for col_i, square in enumerate(row):
                 if square:
                     square.color = color
-                    if col_i >= 0 and col_i < 23:   # check if square matches the next one in the column
+                    if col_i >= 0 and col_i < 24:   # check if square matches the next one in the column
                         if square.check_for_match(self.board[ind_i][col_i + 1]):
+                            self.board[ind_i][col_i + 1].color = color
                             self.matched_squares[ind_i][col_i +
                                                         1] = self.board[ind_i][col_i + 1]
-                            square.color = color
-                            self.board[ind_i][col_i + 1].color = color
-                    if col_i >= 1 and col_i < 24:
-                        # check if square matches the one behind it in the column
+                    if col_i >= 1 and col_i < 24:   # check if square matches the one behind it in the column
                         if square.check_for_match(self.board[ind_i][col_i - 1]):
+                            self.board[ind_i][col_i - 1].color = color
                             self.matched_squares[ind_i][col_i -
                                                         1] = self.board[ind_i][col_i - 1]
-                            square.color = color
-                            self.board[ind_i][col_i - 1].color = color
-                    if ind_i >= 0 and ind_i < 23:    # check if square matches the one below it.
+                    if ind_i >= 0 and ind_i < 24:   # check if square matches the one below it.
                         if square.check_for_match(self.board[ind_i + 1][col_i]):
+                            self.board[ind_i + 1][col_i].color = color
                             self.matched_squares[ind_i +
                                                  1][col_i] = self.board[ind_i + 1][col_i]
-                            square.color = color
-                            self.board[ind_i + 1][col_i].change_color(color)
-                    if ind_i >= 1 and ind_i < 24:    # check if square matches the one above it.
+                    if ind_i >= 1 and ind_i < 25:    # check if square matches the one above it.
                         if square.check_for_match(self.board[ind_i - 1][col_i]):
+                            self.board[ind_i - 1][col_i].color = color
                             self.matched_squares[ind_i -
                                                  1][col_i] = self.board[ind_i - 1][col_i]
-                            square.color = color
-                            self.board[ind_i - 1][col_i].change_color(color)
 
-    def check_winner(self):
-        if None not in self.matched_squares:
-            return self.winner()
+    def check_winner(self, events):
+        for row in self.matched_squares:
+            if None in row:
+                return False
+        return self.winner(events)
 
-    def winner(self):
-        print('You win!')
+    def winner(self, events):
+        button(500 // 2, 500 // 2, 150, 150, (0, 0, 0),
+               (0, 0, 0), events, action=None, msg="You Win!")
+        button(500 // 2, 280, 100, 100, (0, 0, 0), (0, 0, 0),
+               events, self.reset_game, msg="Play Again")
+
+    def check_loser(self, events):
+        if self.attempts_made > self.number_of_tries_total:
+            return self.loser(events)
+
+    def loser(self, events):
+        button(500 // 2, 500 // 2, 150, 150, (0, 0, 0),
+               (0, 0, 0), events, None, "You Lose!")
+        time.sleep(3)
+        self.reset_game()
 
 
 class Square:
@@ -138,45 +146,29 @@ class Square:
         return False
 
 
-# class Button:
-#     def __init__(self, x, y, w, h, inactive, active, action=None):
-#         self.x = x
-#         self.y = y
-#         self.w = w
-#         self.h = h
-#         self.rect = pygame.Rect(x, y, w, h)
-#         self.inactive = inactive
-#         self.active = active
-#         self.action = action
-#         self.pressed = False
-
-#     def press(self):
-#         print('clicked')
-#         self.action(self.inactive)
-
-#     def inactive_func(self):
-#         return main_surface.fill(self.inactive, (self.x, self.y, self.w, self.h))
-
-    # def active_func(self):
-    #     return main_surface.fill(self.active, (self.x, self.y, self.w, self.h))
-
-    # def draw(self):
-    #     main_surface.fill(self.inactive, self.rect)
-
-
-def button(x, y, w, h, inactive, active, events, action=None):
-    click = pygame.mouse.get_pressed()
+def button(x, y, w, h, inactive, active, events, action=None, msg=None):
     button_rect = pygame.Rect(x, y, w, h)
     pos = pygame.mouse.get_pos()
 
+    if msg:
+        largeText = pygame.font.Font('freesansbold.ttf', 40)
+        TextSurf, _ = text_objects(msg, largeText)
+        main_surface.blit(TextSurf, button_rect)
+
     if button_rect.collidepoint(pos):
-        main_surface.fill(active, button_rect)
+        if not msg:
+            main_surface.fill(active, button_rect)
         for event in events:
-            if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.type == pygame.MOUSEBUTTONDOWN and action:
                 action(inactive)
                 clock.tick(60)
-    else:
+    if not msg:
         main_surface.fill(inactive, button_rect)
+
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, (0, 0, 0))
+    return textSurface, textSurface.get_rect()
 
 
 def main():
@@ -192,12 +184,17 @@ def main():
                 pygame.quit()
                 quit()
         game.add_buttons_to_screen(main_surface, events)
+
+        button(50, 600, 200, 200, (0, 0, 0), (0, 0, 0),
+               events, game.reset_game, 'Reset Game')
+        game.check_winner(events)
+        game.check_loser(events)
+
         pygame.display.update()
         clock.tick(60)
-        print(game.attempts_made)
-        if game.attempts_made > 25:
-            game.reset_game()
+
     pygame.quit()
 
 
-main()
+if __name__ == "__main__":
+    main()
